fe6af4e1da5e03148d139f3376c76b97bf6dc59a
Oneikki <823911038@qq.com>
2021-10-18 20:22:48 +0800

    update greedy algo

--- Leetcode/greedy_algo.cc
+++ Leetcode/greedy_algo.cc
@@ -1,7 +1,7 @@
 //leetcode 455 & 135 & 435 & 605
 #include <iostream>
 #include <vector>
-#include <algorithm> //std::max
+#include <algorithm> //std::max, std::sort
 #include <numeric> //std::accumulate
 #include <cstdlib> //size_t
 
@@ -79,7 +79,7 @@ public:
 	bool canPlaceFlowers(std::vector<int>& flowerbed, int n) {
 		for (size_t i = 0; i < flowerbed.size(); ++i) {
 			if (flowerbed[i] == 0 &&
-				(i + 1 == flowerbed.size() || flowerbed[i + 1] == 0)) {
+                (i + 1 == flowerbed.size() || flowerbed[i + 1] == 0)) {
 				--n;
 				i++;
 			}
@@ -89,6 +89,30 @@ public:
 		}
 		return n <= 0;
 	}
+};
+}
+
+namespace hh452 {
+class Solution {
+public:
+    int findMinArrowShots(std::vector<std::vector<int>>& points) {
+        if (points.empty()) {
+            return 0;
+        }
+        std::sort(points.begin(), points.end(),
+                  [](std::vector<int>& v1, std::vector<int>& v2) {
+                  return v1[1] < v2[1];
+                  });
+        int num_arrows = 1;
+        int prev = points[0][1];
+        for (size_t i = 1; i < points.size(); ++i) {
+            if (points[i][0] > prev) {
+                ++num_arrows;
+                prev = points[i][1];
+            }
+        }
+        return num_arrows;
+    }
 };
 }
 
