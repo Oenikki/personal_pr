d8c2de94171f2484fd373d67492226a0aec0567f
Oneikki <823911038@qq.com>
2021-11-08 18:24:05 +0800

    update

--- TinySTL/sub_allocator.h
+++ TinySTL/sub_allocator.h
@@ -81,4 +81,33 @@ void *__mallo_alloc_template<inst>::oom_realloc(void *p, size_t n) {
         }
     }
 }
+
+enum {
+    __ALIGN = 8
+};
+
+enum {
+    __MAX_BYTES = 128
+};
+
+enum {
+    __NFREELISTS = __MAX_BYTES / __ALIGN
+};
+
+template<bool threads, int inst>
+class __default__alloc_template {
+private:
+    static size_t ROUND_UP(size_t bytes) {
+        return ((bytes) + __ALIGN - 1) & ~(__ALIGN - 1);
+    }
+
+private:
+    union obj {
+        union obj *free_list_link;
+        char client_data[1];
+    }
+
+private:
+    
+}
 }
